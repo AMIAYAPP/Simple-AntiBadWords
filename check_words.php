cb831721443c821e5574b39f5d39b6dd3e709449
YagamiiRoot <55007599+YagamiiRoot@users.noreply.github.com>
2020-10-04 01:19:03 +0200

    Update check_words.php

--- check_words.php
+++ check_words.php
@@ -15,7 +15,7 @@ Results:
 Edit the array of the variable $badwords to your own langage
 */
 $wordtocheck = $_GET['txt'];
-$badwords = array('pute','salope','slp','tepu','sale pute','sale put','sale salope','salop','slp','salaud','bounioul','bounioule','bougnoule','sale négro','sale nègre','sale noir','sale nwar','sale nuar','sale nouar','fdp','fils de pute','fils deup','fils de put','fils deux pute','fils deu pute','fils deu put','fils deux put','enculer','enculé','enculée','t\'enculer','batard','btrd','albatard','ntm','nikomok','nique ta mère','nique ta reum','baise ta mère','bztmr','bz ta mère','ta mère la pute','sale gros shlag','sale fils de pute','jv te niquer','jvais te niquer','j\'vais te niquer');
+$badwords = array('pute','salope','slp','tepu','sale pute','sale put','sale salope','salop','salaud','bounioul','bounioule','bougnoule','sale négro','sale nègre','sale noir','sale nwar','sale nuar','sale nouar','fdp','fils de pute','fils deup','fils de put','fils deux pute','fils deu pute','fils deu put','fils deux put','enculer','enculé','enculée','t\'enculer','batard','btrd','albatard','ntm','nikomok','nique ta mère','nique ta reum','baise ta mère','bztmr','bz ta mère','ta mère la pute','sale gros shlag','sale fils de pute','jv te niquer','jvais te niquer','j\'vais te niquer');
 foreach ($badwords as $badw) {
     if (strpos($wordtocheck, $badw) !== false) {
         echo "1"; 
